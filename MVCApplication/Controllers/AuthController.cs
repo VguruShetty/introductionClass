ad1b37d629c5c546e0686168cfe2b1ad1f75dc71
vguru shetty <vgurushetty@gmail.com>
2026-08-19 22:08:15 +0530

    Practice on MVC project

---
+++ MVCApplication/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCApplication.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCApplication/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCApplication.Models;
+
+namespace MVCApplication.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; } // created Schema for user class
+    }
+}
